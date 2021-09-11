da8cddef8793ae4cdb257cec5e8b9c07e7926bee
Saksham Jain <sajain@gammastack.com>
2021-09-11 13:18:09 +0530

    Adds cron

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+require 'open-uri'
 class ApplicationController < ActionController::Base
+  def index
+    request_star_money
+    render plain: :ok
+  end
+
+  private
+
+  def request_star_money
+    URI.open('http://star-money.herokuapp.com?request_type=cron')
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require 'action_controller/railtie'
+require 'sprockets/railtie'
+require "action_view/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
